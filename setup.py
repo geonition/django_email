77ac69c0b11678f5b7f5a0670bfa79ebf84d4f1b
Kristoffer Snabb <kristoffer.snabb@gmail.com>
2011-08-24 22:45:38 +0300

    added setup script

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='email_rest',
+    version='1.0.1',
+    author='Kristoffer Snabb',
+    url='https://github.com/geonition/django_email_rest',
+    packages=['email_rest'],
+)
