13d806a10e30f4128057c464d8c48f7afafc6bf2
Kristoffer Snabb <kristoffer.snabb@gmail.com>
2011-08-30 16:21:08 +0300

    modified setup to include the template files

--- setup.py
+++ setup.py
@@ -1,10 +1,21 @@
-from distutils.core import setup
+from setuptools import setup
+from setuptools import find_packages
 
 setup(
-    name='email_rest',
-    version='1.0.1',
+    name='auth',
+    version='1.0.0',
     author='Kristoffer Snabb',
     url='https://github.com/geonition/django_email_rest',
-    packages=['email_rest'],
-    install_requires=['django'],
+    packages=find_packages(),
+    include_package_data=True,
+    package_data = {
+        "email_rest": [
+            "templates/*.js",
+            "templates/*.html",
+            "templates/emailconfirmation/*.txt",
+            "templates/emailconfirmation/*.html"
+        ],
+    },
+    zip_safe=False,
+    install_requires=['django']
 )
