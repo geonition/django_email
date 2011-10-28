b02f1a7f501baba04f3454a591250f391f78fe84
Kristoffer Snabb <kristoffer.snabb@gmail.com>
2011-10-28 12:02:36 +0300

    the gnt object is now used for the functions

--- email_rest/templates/softgis_email.jquery.js
+++ email_rest/templates/softgis_email.jquery.js
@@ -1,6 +1,39 @@
-function get_email(callback_function){
-     
-     add_CSRF_token_in_request_header();
+/*
+email_rest application javascript functions
+*/
+gnt.email_rest = {};
+
+/*
+helper function to check regular expressions
+*/
+gnt.email_rest.checkRegexp =
+function(o, regexp) {
+    if ( !( regexp.test( o ) ) ) {
+        return false;
+    
+    } else {
+        return true;
+    }
+};
+
+gnt.email_rest.validate_email =
+function(email_address) {
+	if (email_address != null && email_address.length > 6){
+	  isEmailValid = gnt.email_rest.checkRegexp( email_address, /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
+	  if (!isEmailValid){
+	    return false;
+	  }
+	  else {
+	    return true;
+	  }
+	}
+	else{
+	  return false;
+  	}
+};
+
+gnt.email_rest.get_email =
+function(callback_function) {
       
 	$.ajax({
 	  url: '{% url api_manage_email %}',
@@ -18,10 +51,11 @@ function get_email(callback_function){
         },  
 	  dataType: "json"
 	});
-}
+};
 
 
-function save_email(email_address, callback_function){
+gnt.email_rest.save_email =
+function (email_address, callback_function) {
 
 	var email = jQuery.parseJSON(email_address);
 	
@@ -48,7 +82,7 @@ function save_email(email_address, callback_function){
        },  
 	  dataType: "text"
 	});
-}
+};
 
 /*
 typeString
@@ -57,7 +91,8 @@ Default: 'GET'
 The type of request to make ("POST" or "GET"), default is "GET". 
 Note: Other HTTP request methods, such as PUT and DELETE, can also be used here, but they are not supported by all browsers.
 */
-function delete_email(callback_function){
+gnt.email_rest.delete_email =
+function(callback_function) {
 	$.ajax({
 	  url: '{% url api_manage_email %}',
 	  type: "DELETE",
